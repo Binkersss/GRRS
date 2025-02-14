bda0a93892b3c214bd7a5f0acb1470bd0848b582
Nathaniel Chappelle <nathaniel.chappelle@proton.me>
2025-02-13 17:51:26 -0800

    Working correctly, errors a little ugly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::{Context, Result};
+use std::io::BufRead;
+use std::time::Duration;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    // PathBuf is like a string but for cross-platform file system paths
+    path: std::path::PathBuf,
+}
+
+fn main() ->Result<(), Box<dyn std::error::Error>>{
+    let bar = indicatif::ProgressBar::new_spinner();
+    bar.enable_steady_tick(Duration::from_millis(100));
+    
+    let args = Cli::parse();
+    
+    let file = std::fs::File::open(&args.path).with_context(|| format!("could not read file '{}'", args.path.display()))?;
+    let mut reader = std::io::BufReader::new(file);
+    
+    let mut line = String::new();
+    
+    loop {
+        line.clear();
+        
+        let bytes_read = reader.read_line(&mut line).expect("could not read line");
+        
+        if bytes_read == 0 {
+            break;
+        }
+        
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    bar.finish_and_clear();
+    Ok(())
 }
